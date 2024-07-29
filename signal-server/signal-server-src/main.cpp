f2efe72fde3a2c0697dee424a1ca04d627b355b3
andruwa_01 <cheprasov.work@gmail.com>
2024-07-29 09:13:42 +0300

    updated repo with new .gitignore file

--- signal-server/signal-server-src/main.cpp
+++ signal-server/signal-server-src/main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+    QCoreApplication app(argc, argv);
+    std::cout << "hello there" << std::endl;
+    app.exec();
 }
